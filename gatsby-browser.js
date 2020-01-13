cae1f022ee3a51331aa91e0354b11e2661122f5b
Joop Laan <joop.laan@gmail.com>
2020-01-13 11:42:47 +0100

    Initial commit

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+// custom typefaces
+import "./src/style/all.scss"
+import "typeface-montserrat"
+import "typeface-merriweather"
+import "typeface-nunito"
+import "typeface-alegreya"
---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'New York',
+  shortName: 'JL',
+  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
+  url: 'https://www.jooplaan.com', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'jooplaan',
+  twitter: ''
+}
