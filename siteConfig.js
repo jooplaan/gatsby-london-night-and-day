3494f9a6fb125f6c24d6690c5352a5dfd1d190da
Joop Laan <joop.laan@gmail.com>
2020-01-13 13:36:53 +0100

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: 'New York',
-  shortName: 'JL',
+  name: 'London Night and Day',
+  shortName: '',
   description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
   url: 'https://www.jooplaan.com', // Domain of your site without prefix!
   prefix: '/',
